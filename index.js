474676a54ea4292a31c98e8d9ad53b4606fac554
Olanrewaju A. Olaboye <eyiwumi.olaboye@gmail.com>
2024-01-17 17:04:02 +0100

    basic controller

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world!");
+const express = require("express");
+const server = express();
+
+server.get("/", function (req, res) {
+  res.send("<h1>Hello World</h1> <br> <a href='/boye'>Visit Boye</a>");
+});
+
+server.get("/boye", function (req, res) {
+  res.send("<h1>Hello Boye</h1> <br/> <a href='/'>Go back home</a>");
+});
+
+server.listen(3000, function () {
+  console.log("server listening on port 3000");
+});
